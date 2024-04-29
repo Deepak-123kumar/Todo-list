13efb881cf55adbd9e9aec8e817bd78880c5629f
Deepak kumar <152525766+Deepak-123kumar@users.noreply.github.com>
2024-04-29 16:40:20 +0530

    Add files via upload

---
+++ To-do_List/Todo.js
@@ -0,0 +1,26 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value == ""){
+        alert("please enter task")
+    }else{
+        let newEle = document.createElement("ul");
+        
+        newEle.innerHTML=`${inputs.value} `;
+
+        text.appendChild(newEle);
+
+        inputs.value="";
+
+        
+
+        newEle.querySelector("i").addEventListener("click", remove);
+        function remove(){
+            newEle.remove(
+
+
+            )
+        }
+    }
+}
